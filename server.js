258b94e317bcaef66756387cde7cb76f1f9661c8
Trey Burgess <treyb@UNC-PF2AAVE3.localdomain>
2022-09-17 16:12:34 -0400

    no way its been a typo this entire time

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 // Require minimist module (make sure you install this one via npm).
 const minimist = require('minimist')
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const args = process.argv.slice(2)
+const args = require('minimist')(process.argv.slice(2))
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = args.port || 3000
@@ -36,7 +36,7 @@ const server = http.createServer((req, res) => {
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port, () => {
-	console.log('Server listeing on port ${port}')
+	console.log('Server listening on port ${port}')
 })
 
 // That's it! You're all done!
