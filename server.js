f1485d9b9acd1a1e65516c2b80a50537cfca6a33
Trey Burgess <treyb@UNC-PF2AAVE3.localdomain>
2022-11-02 13:56:31 -0400

    changed server listening no arrow function

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const fs = require('fs')
 const minimist = require('minimist')
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = require('minimist')(process.argv.slice(2))
-args["port"]
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = args.port || process.env.PORT || 3000
@@ -36,8 +35,8 @@ const server = http.createServer((req, res) => {
 })
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(port, () => {
-	console.log(`Server listening on port ${port}`)
-})
+server.listen(port)
+console.log(`Server listening on port ${port}`)
+
 
 // That's it! You're all done!
